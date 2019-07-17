8bddf270df32d00bae3938760cd1f94fafe7a1e3
Marian Ferenc <marian.ferenc@student.tuke.sk>
2019-07-17 18:57:18 +0200

    initial maven project

---
+++ src/main/java/com/ibm/Main.java
@@ -0,0 +1,7 @@
+package com.ibm;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("com.ibm.Main works!");
+    }
+}
---
+++ src/test/java/com/ibm/FunctionalityTest.java
@@ -0,0 +1,11 @@
+package com.ibm;
+
+import org.junit.Test;
+
+public class FunctionalityTest {
+
+    @Test
+    public void test1(){
+        System.out.println("Test1 works!");
+    }
+}
